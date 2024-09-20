32c091d22b0223c14a6a963f8711d295d317ae9c
Daniel Souza <danielsouzaaj@hotmail.com>
2024-09-19 22:59:29 -0300

    feat: slider

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import { register } from 'swiper/element/bundle';
+import './index.css';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import 'swiper/css/effect-fade'
+register();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
