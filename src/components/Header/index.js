f55d83a4ac384f4160bd02abbdad1e1a380596ff
Daniel Souza <danielsouzaaj@hotmail.com>
2024-10-18 00:59:40 -0300

    Updated header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,12 @@
 import { Link } from 'react-router-dom';
-import { FaFilm, FaYoutube } from 'react-icons/fa';
+import { FaFilm } from 'react-icons/fa';
 import './header.css';
 
 function Header() {
     return (
         <header>
             <div className='container'>
-                <Link className='logo' to='/'>{ <FaYoutube/> } MovieFlix</Link>
+                <Link className='logo' to='/'>MovieFlix</Link>
                 <Link className='favoritos' to='/favoritos'>{ <FaFilm/> } Meus Filmes</Link>
             </div>
         </header>
