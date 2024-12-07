1dd8602c4e20ceff334257b13208561ae25eba90
Daniel Souza <danielsouzaaj@hotmail.com>
2024-12-07 20:18:37 -0300

    feat: atualizando

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
     baseURL: "https://api.themoviedb.org/3/",
     params: {
-        api_key: process.env.REACT_APP_API_KEY,
+        api_key: 'a6537545953aa1da6e334f5e976d4caa',
         language: 'pt-BR',
     }
 })
